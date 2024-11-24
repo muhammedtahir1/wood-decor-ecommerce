8a9629dd7dbc40bd0d169c6ce544c069f9d682e1
Mohammed Faizan <mohdfaizan.zprb2@gmail.com>
2024-11-24 16:51:11 +0530

    /admin/products server side pagination working ✅

--- src/app/(admin)/admin/products/page.tsx
+++ src/app/(admin)/admin/products/page.tsx
@@ -1,58 +1,77 @@
 import prisma from "@/lib/db";
-import React from "react";
 import AdminProductsPagination from "@/components/admin/admin-products-pagination";
-import { validateUrl } from "@/lib/utils";
 import { ProductWithVariants } from "@/types/validations";
 import AdminBreadCrumbComponent from "@/components/admin/admin-breadcrumb";
 import { revalidatePath } from "next/cache";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Ellipsis } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import DeleteProduct from "@/components/admin/delete-product";
+import AddProductForm from "@/components/admin/admin-form";
 
-const Page = async ({ searchParams }: { searchParams: { page: string, title?: string } }) => {
-
-  const { page, title } = searchParams
-
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { page: string; title?: string };
+}) => {
+  // Getting the page number from the query params
+  const { page, title } = searchParams;
   const TABLE_PAGINATION_LIMIT = 10;
-
   let pageNumber: number;
 
   try {
-    pageNumber = parseInt(page) || 1
-    if (pageNumber < 1) pageNumber = 1
+    pageNumber = parseInt(page) || 1;
+    if (pageNumber < 1) pageNumber = 1;
   } catch (error) {
-    pageNumber = 1
+    pageNumber = 1;
   }
 
-  const paginationProducts: ProductWithVariants[] = await prisma.product.findMany({
-    where: {
-      title: title ? {
-        contains: title,
-        mode: "insensitive"
-      } : undefined
-    },
-    skip: (pageNumber - 1) * TABLE_PAGINATION_LIMIT,
-    take: TABLE_PAGINATION_LIMIT,
-    include: {
-      prices: true
-    }
+  // Fetching the products from the database
+  const paginationProducts: ProductWithVariants[] =
+    await prisma.product.findMany({
+      where: {
+        title: title
+          ? {
+              contains: title,
+              mode: "insensitive",
+            }
+          : undefined,
+      },
+      skip: (pageNumber - 1) * TABLE_PAGINATION_LIMIT,
+      take: TABLE_PAGINATION_LIMIT,
+      include: {
+        prices: true,
+      },
+    });
+  // revalidatePath(`/admin/products?page=${pageNumber}?title=${title}`);
 
-  })
-  revalidatePath(`/admin/products?page=${pageNumber}?title=${title}`);
-
-
-
+  // Getting the total number of products
   const totalProducts = await prisma.product.count({
     where: {
-      title: title ? {
-        contains: title,
-        mode: "insensitive"
-      } : undefined
-    }
+      title: title
+        ? {
+            contains: title,
+            mode: "insensitive",
+          }
+        : undefined,
+    },
   });
 
+  // Checking if there are more products that can be fetched from the database
   const canNextPage = totalProducts > TABLE_PAGINATION_LIMIT * pageNumber;
   const canPrevPage = pageNumber > 1;
-  const totalPages = Math.ceil(totalProducts / TABLE_PAGINATION_LIMIT)
-
-
+  const totalPages = Math.ceil(totalProducts / TABLE_PAGINATION_LIMIT);
 
   return (
     <main>
@@ -60,6 +79,38 @@ const Page = async ({ searchParams }: { searchParams: { page: string, title?: st
         <AdminBreadCrumbComponent slug="Products" />
         <h1 className="mt-2">All Products</h1>
         {/* <ProductVariantManager /> */}
+        <section>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[300px]">Name</TableHead>
+                <TableHead>Category</TableHead>
+                <TableHead className="text-right">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody className="overflow-x-scroll">
+              {paginationProducts.map((product, i) => (
+                <TableRow key={i}>
+                  <TableCell className="font-medium w-96">
+                    {product.title}
+                  </TableCell>
+                  <TableCell>{product.slug}</TableCell>
+                  <TableCell className="text-right flex">
+                    <Popover>
+                      <PopoverTrigger>
+                        <Ellipsis />
+                      </PopoverTrigger>
+                      <PopoverContent className="w-36 bg-transparent space-y-2 border-none">
+                        <DeleteProduct id={product.id} />
+                        <AddProductForm actionType="edit" data={product} />
+                      </PopoverContent>
+                    </Popover>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </section>
         <AdminProductsPagination
           initialProducts={paginationProducts}
           canNextPage={canNextPage}
--- src/components/admin/admin-products-pagination.tsx
+++ src/components/admin/admin-products-pagination.tsx
@@ -2,140 +2,49 @@
 
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Product } from "@prisma/client";
-import { ImSpinner6 } from "react-icons/im";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Ellipsis } from "lucide-react";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import DeleteProduct from "./delete-product";
-import AddProductForm from "./admin-form";
-import { getProducts } from "@/actions/admin.action";
 import { ProductWithVariants } from "@/types/validations";
-import { columns, DataTable } from "./data-table";
-
-// const NUMBER_OF_PRODUCTS_TO_FETCH = 8;
+import { useRouter, useSearchParams } from "next/navigation";
 
 type AdminProductsProps = {
   initialProducts: ProductWithVariants[];
   canNextPage: boolean;
   canPrevPage: boolean;
-  totalPages : number;
-  pageNumber: number
-}
+  totalPages: number;
+  pageNumber: number;
+};
 
 const AdminProductsPagination = ({
   initialProducts,
   canNextPage,
   canPrevPage,
   pageNumber,
-  totalPages
+  totalPages,
 }: AdminProductsProps) => {
-  const [products, setProducts] =
-    useState<ProductWithVariants[]>(initialProducts);
-  // const [offset, setOffset] = useState(NUMBER_OF_PRODUCTS_TO_FETCH);
-  const [loading, setLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     setLoading(true);
-  //     try {
-  //       const apiProducts = await getProducts({
-  //         take: NUMBER_OF_PRODUCTS_TO_FETCH,
-  //         skip: 0,
-  //       });
-  //       console.log("Fetched products:", apiProducts);
-  //       setProducts(apiProducts);
-  //       setOffset(NUMBER_OF_PRODUCTS_TO_FETCH);
-  //       setHasMore(apiProducts.length === NUMBER_OF_PRODUCTS_TO_FETCH);
-  //     } catch (error) {
-  //       console.error("Failed to load products", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-  //   fetchProducts();
-  // }, []); // Remove offset from the dependency array
-
-  // const loadMoreProducts = async () => {
-  //   if (!hasMore) return;
-
-  //   setLoading(true);
-  //   try {
-  //     const apiProducts = await getProducts({
-  //       take: NUMBER_OF_PRODUCTS_TO_FETCH,
-  //       skip: offset,
-  //     });
-  //     if (apiProducts.length < NUMBER_OF_PRODUCTS_TO_FETCH) {
-  //       setHasMore(false);
-  //     }
-  //     setProducts((prevProducts) => [...prevProducts, ...apiProducts]);
-  //     setOffset((prevOffset) => prevOffset + NUMBER_OF_PRODUCTS_TO_FETCH);
-  //   } catch (error) {
-  //     console.error("Failed to load more products", error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
+  const page = searchParams.get("page") ?? "1";
 
   return (
     <div className="min-h-[90vh] pt-2 px-1 mt-2 mb-4 md:mb-6">
-      {/* <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[300px]">Name</TableHead>
-            <TableHead>Category</TableHead>
-            <TableHead className="text-right">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody className="overflow-x-scroll">
-          {products.map((product, i) => (
-            <TableRow key={i}>
-              <TableCell className="font-medium w-96">
-                {product.title}
-              </TableCell>
-              <TableCell>{product.slug}</TableCell>
-              <TableCell className="text-right flex">
-                <Popover>
-                  <PopoverTrigger>
-                    <Ellipsis />
-                  </PopoverTrigger>
-                  <PopoverContent className="w-36 bg-transparent space-y-2 border-none">
-                    <DeleteProduct id={product.id} />
-                    <AddProductForm actionType="edit" data={product} />
-                  </PopoverContent>
-                </Popover>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table> */}
-
-      <DataTable columns={columns} data={products} canNextPage={canNextPage} canPrevPage={canPrevPage} pageNumber={pageNumber} totalPages={totalPages}/>
-
-      {/* {loading && (
-        <div className="flex justify-center items-center mt-4">
-          <ImSpinner6 className="animate-spin" size={24} />
-        </div>
-      )}
-
-      {!loading && hasMore && (
-        <Button onClick={loadMoreProducts} className="mb-10 md:mb-20">
-          Load More...
+      <div className="flex justify-between gap-0 items-center mt-4">
+        <Button
+          onClick={() => {
+            router.push(`/admin/products?page=${Number(page) - 1}`);
+          }}
+          disabled={!canPrevPage}
+        >
+          Previous
         </Button>
-      )} */}
+        <Button
+          onClick={() => {
+            router.push(`/admin/products?page=${Number(page) + 1}`);
+          }}
+          disabled={!canNextPage}
+        >
+          Next
+        </Button>
+      </div>
     </div>
   );
 };
